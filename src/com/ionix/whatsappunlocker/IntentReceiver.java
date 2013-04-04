22747a4dfe9411852170f5064287b6a714ed475c
Andreas Pohl <apohl79@gmail.com>
2013-04-04 22:48:56 +0200

    Fixed the case that the unlocker is already running
    Signed-off-by: Andreas Pohl <apohl79@gmail.com>

--- src/com/ionix/whatsappunlocker/IntentReceiver.java
+++ src/com/ionix/whatsappunlocker/IntentReceiver.java
@@ -8,13 +8,12 @@ import android.content.Intent;
 import android.os.PowerManager;
 import android.util.Log;
 
-
 public class IntentReceiver extends BroadcastReceiver {
 
 	static final String TAG = "WhatsAppUnlocker";
-	
+
 	private static KeyguardLock m_klock = null;
-	
+
 	@Override
 	public void onReceive(Context ctx, Intent intent) {
 		String comp = intent.getStringExtra("comp");
@@ -23,6 +22,10 @@ public class IntentReceiver extends BroadcastReceiver {
 			// unlock lockscreen
 			KeyguardManager km = (KeyguardManager) ctx.getSystemService(Context.KEYGUARD_SERVICE);
 			if (km.inKeyguardRestrictedInputMode()) {
+				if (null != m_klock) {
+					m_klock.reenableKeyguard();
+					m_klock = null;
+				}
 				Log.v(TAG, "IONIX: Unlocking screen for WhatsApp");
 				m_klock = km.newKeyguardLock(TAG);
 				m_klock.disableKeyguard();
